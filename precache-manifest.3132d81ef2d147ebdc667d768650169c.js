9b3c4df8dad6b523d887d4139cbd70b5f6ec8f10
Alan Torres <alantorres@slack-corp.com>
2019-10-21 20:15:54 -0700

    Updates

---
+++ precache-manifest.3132d81ef2d147ebdc667d768650169c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ad29a272f41dbeed1ba6b0ceeb2032c",
+    "url": "/scriptgeeky/tic-tac-toe.git/index.html"
+  },
+  {
+    "revision": "ddbc4ed9f14e690423f5",
+    "url": "/scriptgeeky/tic-tac-toe.git/static/css/main.382f9f21.chunk.css"
+  },
+  {
+    "revision": "d10c2d5dcbd971ac4824",
+    "url": "/scriptgeeky/tic-tac-toe.git/static/js/2.5b22e848.chunk.js"
+  },
+  {
+    "revision": "ddbc4ed9f14e690423f5",
+    "url": "/scriptgeeky/tic-tac-toe.git/static/js/main.dce142e0.chunk.js"
+  },
+  {
+    "revision": "96abc3f355865bef0fce",
+    "url": "/scriptgeeky/tic-tac-toe.git/static/js/runtime-main.ed1b5cc7.js"
+  }
+]);
